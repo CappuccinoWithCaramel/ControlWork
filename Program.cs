38738495c0091934d1179480c12fa5454044fe6e
Stanislav <neruhin2001@gmail.com>
2023-03-25 20:51:48 +0300

    Создание первичного тела программы

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] Array(int size)
+{
+    string[] Array= new string[size];
+    for (int i=0; i< size; i++)
+    {
+        Array[i]=Convert.ToString(Console.ReadLine())!;   
+    }
+    return Array;
+}
+void ShowArray(string[]Array)
+{
+
+    for (int i=0; i< Array.Length; i++)
+    {
+        Console.Write(Array[i] + ", ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Input Value of array");
+int size=Convert.ToInt32(Console.ReadLine());
+
+
+string[] myArray = Array(size);
+ShowArray(myArray);
+Console.WriteLine();
+Searching(myArray);
